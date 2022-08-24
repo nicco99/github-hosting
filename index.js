0c7da1a7d420e77a44380a0f06bb32eadd366bef
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-08-24 17:48:20 +0300

    new

---
+++ index.js
@@ -0,0 +1,23 @@
+const div = document.querySelector("#days");
+// console.log(div)
+
+// const week = ["monday","tuesday","wednesday","thurday","friday","saturday","sunday"];
+// let p = document.createElement("p")
+// p.innerText = week.join("||")
+// div.appendChild(p)
+
+// const weekend = week.splice(5,2,);
+
+// let ourWeekend = document.createElement("h3")
+// ourWeekend.innerText = `our weekend days are ${weekend.join(" and ")} and our working days are ${week.join("||")}`;
+// div.appendChild(ourWeekend)
+
+function printName() {
+const p = document.createElement("p");
+p.innerText = "i love you Harry ❤️";
+const div = document.getElementById("days")
+div.appendChild(p)
+    printName();
+    }
+
+document.getElementById("print").addEventListener("click", printName)
